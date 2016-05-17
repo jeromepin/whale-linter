44dce067e248b74b5e1158c16705d40e2d82deb4
Jerome Pin <jerome@jeromepin.fr>
2016-05-17 21:34:25 +0200

    Delete unused "remove" method

--- whalelinter/app.py
+++ whalelinter/app.py
@@ -4,15 +4,6 @@ import json
 
 
 class App:
-    # @staticmethod
-    # def remove(lst, element):
-    #     ret = False
-    #     if element in lst:
-    #         lst.remove(element)
-    #         ret = True
-    #
-    #     return ret
-
     @staticmethod
     def load_configuration():
         config_file = os.path.dirname(os.path.realpath(__file__)) + '/config.json'
