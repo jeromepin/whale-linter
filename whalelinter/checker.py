f65fd9161189caf4c3a3865d59b6950e8071a60a
Jerome Pin <jerome@jeromepin.fr>
2016-06-27 09:35:20 +0200

    Make Dispatcher object local

--- whalelinter/checker.py
+++ whalelinter/checker.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 from whalelinter.app          import App
 from whalelinter.dispatcher   import Dispatcher
+from whalelinter.token        import Maintainer
 
 class Checker(object):
     def __init__(self, dictionnary):
         self.lines              = dictionnary
-        self.dispatcher         = Dispatcher(self)
+        self.line_number        = 0
         self.consecutive_run    = 0
         self.apt_has_been_used  = False
 
-    def all(self):
-        for lineno, line in self.lines.items():
-            self.lineno = lineno
-            self.dispatcher.react(line, self.lineno)
+    def check(self):
+        dispatcher = Dispatcher()
+
+        for self.line_number, line in self.lines.items():
+            dispatcher.react(line, self.line_number)
             self.must_be_present(line[0])
             self.must_be_unique(line[0])
             self.should_be_present(line[0])
