53eb3c51e9f9a4c59d51e71a5ec49592c41376e8
Jerome Pin <jerome@jeromepin.fr>
2016-06-27 09:30:34 +0200

    Move all method from Dispatcher registering to internal registering

--- whalelinter/commands/apt.py
+++ whalelinter/commands/apt.py
@@ -17,47 +17,42 @@ class Apt(PackageManager):
 
         return ret
 
-    @Dispatcher.register(token='run', command='apt-get')
-    def this(checker, command):
-        checker.apt_has_been_used = True
-        # filter global apt-get params
-        if ('update' not in command) and Apt.remove(command, '-y'):
-            App._collecter.throw(2010, checker.lineno, keys={'command': 'apt-get ' + ' '.join(command)})
-
-    def update(checker):
-        pass
-
-    @Dispatcher.register(token='run', command='apt-get', subcommand='upgrade')
-    def upgrade(checker, packages):
-        App._collecter.throw(2008, checker.lineno)
-
-        for idx, package in enumerate(packages):
     _callbacks = {}
 
     def __init__(self, token, command, args, line):
         PackageManager.__init__(self, token, command, args, line)
+
+        Apt.register(self)(type(self).install)
+        Apt.register(self)(type(self).is_parameter_present, name='install', parameter='-y', args=args)
+        Apt.register(self)(type(self).is_parameter_present, name='install', parameter='--no-install-recommends', args=args)
+        Apt.register(self)(type(self).upgrade)
+        Apt.register(self)(type(self).dist_upgrade, name='dist-upgrade')
+
+        for method in self.methods:
+            if self.subcommand == method:
+                self.react(method)
+
+    def install(self):
+        for idx, package in enumerate(self.packages):
             if '=' not in package:
-                App._collecter.throw(3003, checker.lineno, keys={'package': package})
+                App._collecter.throw(3003, self.line_number, keys={'package': package})
+            else:
+                self.packages[idx] = package.split('=')[0]
+
+        if sorted(self.packages) != self.packages:
+            App._collecter.throw(3002)
+
+    def upgrade(self):
+        App._collecter.throw(2008, self.line_number)
+
+        for idx, package in enumerate(self.packages):
+            if '=' not in package:
+                App._collecter.throw(3003, self.line_number, keys={'package': package})
             else:
                 packages[idx] = package.split('=')[0]
 
-        if packages and sorted(packages) == packages:
+        if self.packages and sorted(self.packages) == self.packages:
             App._collecter.throw(3002)
 
-    @Dispatcher.register(token='run', command='apt-get', subcommand='install')
-    def install(checker, packages):
-        if Apt.remove(packages, '--no-install-recommends'):
-            App._collecter.throw(2009, checker.lineno, keys={'command': ' '.join(command)})
-
-        for idx, package in enumerate(packages):
-            if '=' not in package:
-                App._collecter.throw(3003, checker.lineno, keys={'package': package})
-            else:
-                packages[idx] = package.split('=')[0]
-
-        if sorted(packages) != packages:
-            App._collecter.throw(3002)
-
-    @Dispatcher.register(token='run', command='apt-get', subcommand='dist-upgrade')
-    def dist_upgrade(checker, packages):
-        App._collecter.throw(2011, checker.lineno)
+    def dist_upgrade(self):
+        App._collecter.throw(2011, self.line_number)
