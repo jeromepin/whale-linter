af010c5e924a779a37495905efc32aecdfd358ea
Jerome Pin <jerome@jeromepin.fr>
2017-04-29 12:17:54 +0200

    Fix line addressing issue on 'cd' command

--- whalelinter/commands/common.py
+++ whalelinter/commands/common.py
@@ -10,8 +10,7 @@ from whalelinter.commands.apt       import Apt
 @Dispatcher.register(token='run', command='cd')
 class Cd(ShellCommand):
     def __init__(self, **kwargs):
-        App._collecter.throw(2002, self.line)
-        return False
+        App._collecter.throw(2002, kwargs.get('lineno'))
 
 
 @Dispatcher.register(token='run', command='rm')
